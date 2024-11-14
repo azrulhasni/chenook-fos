c3123da0d9eac3058584a30aca8fe30aeb7df7a3
Azrul <azrulhasni@gmail.com>
2024-11-14 22:02:03 +0800

    * Make groovy pre-run post-run work * Signature as embedded component

--- src/main/frontend/generated/vaadin.ts
+++ src/main/frontend/generated/vaadin.ts
@@ -33,7 +33,3 @@ import './index';
 
 import './vaadin-react.js';
 import 'Frontend/generated/jar-resources/vaadin-dev-tools/vaadin-dev-tools.js';
-
-import './theme-chenook.global.generated.js';
-import { applyTheme } from './theme.js';
-applyTheme(document);
